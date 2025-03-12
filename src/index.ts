fa12dae0f9708e4a9091d14bd0aa3d7d48c985a1
kesavan200314 <vijaykesa1114@gmil.com>
2025-03-12 15:42:50 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(cookieParser())
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5175'],  // Your frontend URL
+  origin: ['http://localhost:5173', 'http://localhost:5174'],  // Your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,  // Allow cookies if needed
 }));
